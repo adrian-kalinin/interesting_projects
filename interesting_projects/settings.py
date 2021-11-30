4b6df2c6ad02993043a7da89a836cccb6bd33508
Adrian Kalinin <adriankalinin@protonmail.com>
2021-12-01 00:27:02 +0200

    Create projects app.

--- interesting_projects/settings.py
+++ interesting_projects/settings.py
@@ -22,7 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+
+    'apps.projects'
 ]
 
 MIDDLEWARE = [
