e1cd776b00a442e2ddd1ca7eafb41867a766f2b4
Adrian Kalinin <adriankalinin@protonmail.com>
2021-12-01 17:56:35 +0200

    Fix creating project entry.

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.response import Response
 
 from .models import ProjectEntry
 from .serializers import ProjectEntrySerializer
@@ -11,3 +12,15 @@ class ProjectEntryViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectEntrySerializer
 
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def create(self, request, *args, **kwargs):
+        project_entry = ProjectEntry.objects.create(
+            name=request.data['name'],
+            description=request.data['description'],
+            link=request.data['link'],
+            rating=request.data['rating'],
+            owner=request.user
+        )
+
+        serializer = self.get_serializer(project_entry)
+        return Response(serializer.data)
